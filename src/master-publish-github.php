9cdb504135a67d330e2dea2f9b00a2f561aa4f28
Stefan Niederhauser <ghuder5@gmx.ch>
2014-12-28 06:22:14 +0100

    error corrections

--- src/master-publish-github.php
+++ src/master-publish-github.php
@@ -1,3 +1,5 @@
 <?php
 $gitUrl = json_decode($_POST['payload'], true)['repository']['url'];
-file_get_contents("http://localhost/master-publish.php?gitUrl=" . $gitUrl . '&start=' . $_GET['start'] . '&stop=' . $_GET['stop']);
+$start = $_GET['start'] !== null ? '&start' : '';
+$stop = $_GET['stop'] !== null ? '&stop' : '';
+file_get_contents('http://localhost/master-publish.php?gitUrl=' . $gitUrl . $start . $stop);

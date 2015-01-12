7dd6ffa905f3ab5d44a9f695d50696c0b85193ec
Stefan Niederhauser <ghuder5@gmx.ch>
2015-01-12 19:27:00 +0100

    add support for .s3

--- src/publish.php
+++ src/publish.php
@@ -1,9 +1,18 @@
 <?php
 /**
  * Get the contents of the git repo at $_GET['gitUrl'], run jekyll and publish it on an amazon s3.
+ *
  * Required files (see config/.aws):
  *   ~/.aws/config containing the aws key, secret and region as used by aws cli tools.
  *   ~/.aws/profiles containing profiles with git url, git credentials the aws s3 bucket to deploy the pages to.
+ *
+ * If the repo contains a file named '.s3' it is used to configure some meta data.
+ * An example .s3 file is:
+ *   assets/* = expires:100d,delete
+ *   * = expires:1d,size-only
+ * This defines the following:
+ *   Files in the asset folder expire in 100 days and files existing in s3 but not in the repo are deleted
+ *   All other files expire in 1 day and only the file size is checked to see if it must be copied to s3.
  */
 
 require 'Profile.php';
@@ -23,33 +32,70 @@ processRequest('Processing publish request for "' . $_GET['gitUrl'], function ()
     }
 
     execute("cd $dest; jekyll build");
-    $cache_configs = calc_caching($dest);
-    foreach ($cache_configs as $cache_config) {
-        execute("aws s3 sync $dest/_site {$profile->awsBucket} --delete --size-only {$cache_config}");
+    $s3_configs = calc_s3_configs($dest);
+    foreach ($s3_configs as $s3_config) {
+        execute("aws s3 sync $dest/_site {$profile->awsBucket} {$s3_config}");
     }
 });
 
-function calc_caching($dir)
+function calc_s3_configs($dir)
 {
-    $config = $dir . '/.caching';
     $res = array();
+    $config = $dir . '/.s3';
     if (file_exists($config) && $lines = parse_ini_file($config)) {
-        foreach ($lines as $pattern => $expires) {
-            $res[] = calc_cludes($lines, $pattern) . ' --expires=' . parse_expires($expires);
+        foreach ($lines as $pattern => $flags) {
+            $res[] = calc_cludes($lines, $pattern) . ' ' . parse_flags($flags);
         }
+    } else {
+        $res[] = '--delete --size-only';
     }
     return $res;
 }
 
 function calc_cludes($lines, $pattern)
 {
-    $cludes = '--include="' . $pattern . '"';
+    $cludes = '';
     foreach ($lines as $key => $value) {
         if ($key !== $pattern) {
-            $cludes .= ' --exclude="' . $key . '"';
+            $cludes .= '--exclude="' . $key . '" ';
         }
     }
-    return $cludes;
+    return $cludes . '--include="' . $pattern . '"';
+}
+
+function parse_flags($flags)
+{
+    $s = '';
+    $fl = explode(',', $flags);
+    foreach ($fl as $flag) {
+        $kv = explode(':', $flag, 2);
+        if (count($kv) === 1) {
+            $kv[] = '';
+        }
+        list($key, $value) = $kv;
+        switch ($key) {
+            case 'expires':
+                $value = parse_expires($value);
+                break;
+            case 'size-only':
+            case 'content-type':
+            case 'cache-control':
+            case 'content-disposition':
+            case 'content-encoding':
+            case 'content-language':
+            case 'exact-timestamps':
+            case 'delete':
+                break;
+            default:
+                error_log("Ignoring unknown option '$key'");
+                $key = '';
+        }
+        if ($key) {
+            $s .= '--' . $key . ($value ? ('="' . $value . '"') : '') . ' ';
+        }
+    }
+    echo $s;
+    return $s;
 }
 
 function parse_expires($e)
